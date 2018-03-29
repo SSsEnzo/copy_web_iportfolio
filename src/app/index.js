a281c8fa8e46a0f57c3027b5360f3a1ce57ffd37
이준호 <junho@ijunhoui-MacBook-Pro.local>
2018-03-29 12:04:59 +0900

    change path for stateful components

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/main';
+import Main from './containers/main';
 
 
 ReactDOM.render(<Main />, document.getElementById('app'));
